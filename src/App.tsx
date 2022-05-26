f1a7e0d41b2ed0148038a09609be49ef262c066c
vadimition <vadym.sirenko.triare@gmail.com>
2022-05-26 17:42:21 +0300

    eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import Layout from "./loyout/Layout";
+import Layout from './loyout/Layout';
 
 function App() {
   return (
-      <div>
-        <Layout/>
-      </div>
+    <div>
+      <Layout />
+    </div>
   );
 }
 

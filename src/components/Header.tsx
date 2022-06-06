675df1a75235350f0cfcfd827cf9d1803c617345
melkabulka <78478572+melkabulka@users.noreply.github.com>
2022-06-06 20:38:50 +0300

    heder modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import logo from '../images/logo.png';
 import styles from '../style/header.module.scss';
 import buttonNav from '../images/buttonNav.svg';
@@ -8,29 +9,42 @@ interface IHeader {
   isAuth: boolean;
 }
 
-const Header = ({ setOpen, isAuth }:IHeader):JSX.Element => (
-  <header className={styles.header}>
-    <div className={styles.header_item}>
-      {!isAuth && (
-        <button className={styles.navigationButton} type="button" onClick={setOpen}>
-          <img src={buttonNav} alt="menu" />
-        </button>
-      )}
-      <img className={styles.logo} src={logo} alt="logo" />
-    </div>
-    {!isAuth && (
-      <div className={styles.header_item}>
-        <span className={styles.user}>name</span>
-        <button
-          className={styles.avatarka}
-          type="button"
-        >
-          NV
-        </button>
-      </div>
-    )}
+const Header = ({ setOpen, isAuth }:IHeader):JSX.Element => {
+  const [openModal, setOpenModal] = useState(false);
 
-  </header>
-);
+  const open = () => {
+    setOpenModal(!openModal);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header_item}>
+        {!isAuth && (
+          <button className={styles.navigationButton} type="button" onClick={setOpen}>
+            <img src={buttonNav} alt="menu" />
+          </button>
+        )}
+        <img className={styles.logo} src={logo} alt="logo" />
+      </div>
+      {!isAuth && (
+        <div className={styles.header_item}>
+          <span className={styles.user}>name</span>
+          <button
+            className={styles.avatarka}
+            type="button"
+            onClick={open}
+          >
+            NV
+          </button>
+          <div className={clsx(styles.avatarka__modal, openModal && styles.avatarka__modal__open)}>
+            <div><a href="/login" className={styles.avatarka__modal__item}>змінити пароль</a></div>
+            <div><a href="/login" className={styles.avatarka__modal__item}>вийти</a></div>
+          </div>
+        </div>
+      )}
+
+    </header>
+  );
+};
 
 export default Header;

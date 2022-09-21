49c130046f6b238ecbfd0f265eefd64dc3665f7c
Yaroslav-Solomyaniy <Soman74geeev@gmail.com>
2022-09-21 14:27:08 +0300

    fix

--- src/pages/Group/index.tsx
+++ src/pages/Group/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import TitlePage from '../../components/TitlePage';
 import Button from '../../components/common/Button/index';
 import styles from './index.module.scss';

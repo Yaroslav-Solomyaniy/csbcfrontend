acb6c9823f437cc0343c57dedd9d63655d050fad
Yaroslav Solomyaniy <Soman74geeev@gmail.com>
2022-11-04 10:51:50 +0200

    Add normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './Routes';
 import DeviceSizeProvider from './context/All/DeviceType';
+import './normalize.css';
 
 const App = (): JSX.Element => (
   <BrowserRouter>
